d5a75a9953d1fe89a97bb5aee5a497d4f736dc3d
safu9 <safu9.dev@gmail.com>
2018-10-01 21:31:08 +0900

    Add menu

--- src/main/index.js
+++ src/main/index.js
@@ -1,17 +1,19 @@
 'use strict'
 
-import { app, BrowserWindow } from 'electron'
+import { app, BrowserWindow, Menu } from 'electron'
+
+const isDevelopment = (process.env.NODE_ENV === 'development')
 
 /**
  * Set `__static` path to static files in production
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
  */
-if (process.env.NODE_ENV !== 'development') {
+if (!isDevelopment) {
   global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
 }
 
 let mainWindow
-const winURL = process.env.NODE_ENV === 'development'
+const winURL = isDevelopment
   ? `http://localhost:9080`
   : `file://${__dirname}/index.html`
 
@@ -24,7 +26,7 @@ function createWindow () {
     useContentSize: true,
     width: 1000,
     webPreferences: {
-      webSecurity: process.env.NODE_ENV !== 'development'
+      webSecurity: !isDevelopment
     }
   })
 
@@ -33,6 +35,48 @@ function createWindow () {
   mainWindow.on('closed', () => {
     mainWindow = null
   })
+
+  const template = [
+    {
+      label: 'File',
+      submenu: [
+        {
+          label: 'Open File',
+          accelerator: 'CmdOrCtrl + O'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Quit',
+          accelerator: 'CmdOrCtrl + Q',
+          click () {
+            app.quit()
+          }
+        }
+      ]
+    }
+  ]
+
+  if (isDevelopment) {
+    template.push({
+      label: 'Dev',
+      submenu: [
+        {role: 'reload'},
+        {role: 'forcereload'},
+        {role: 'toggledevtools'},
+        {type: 'separator'},
+        {role: 'resetzoom'},
+        {role: 'zoomin'},
+        {role: 'zoomout'},
+        {type: 'separator'},
+        {role: 'togglefullscreen'}
+      ]
+    })
+  }
+
+  let appMenu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(appMenu)
 }
 
 app.on('ready', createWindow)
