c595c1ad07369a8d74819fdfe542b5111fd12381
safu9 <safu9.dev@gmail.com>
2018-10-01 20:51:49 +0900

    Add error handling

--- src/renderer/views/LandingPage.vue
+++ src/renderer/views/LandingPage.vue
@@ -9,11 +9,9 @@
       <hr>
       <p><button @click="openFile">Open file</button></p>
 
-      <div v-show="metadata.title">
-        <button @click="togglePlay"><SvgIcon :icon="isPlaying ? 'pause' : 'play'"></SvgIcon></button>
-        <p>{{ metadata.title }} / {{ metadata.artist }}</p>
-        <p><img :src="metadata.picture" /></p>
-      </div>
+      <button @click="togglePlay"><SvgIcon :icon="isPlaying ? 'pause' : 'play'"></SvgIcon></button>
+      <p>{{ metadata.title || filePath }}<span v-show="metadata.artist"> / {{ metadata.artist }}</span></p>
+      <p><img :src="metadata.picture" /></p>
     </main>
   </div>
 </template>
@@ -60,21 +58,26 @@
 
         mm.parseFile(this.filePath, {native: true})
           .then(metadata => {
-            let pic = metadata.common.picture[0]
-            metadata.common.picture = 'data:' + pic.format + ';base64,' + pic.data.toString('base64')
+            if (metadata.common.picture) {
+              let pic = metadata.common.picture[0]
+              metadata.common.picture = 'data:' + pic.format + ';base64,' + pic.data.toString('base64')
+            }
 
             this.metadata = metadata.common
           })
           .catch(err => {
-            console.error(err.message)
+            console.log(err.message)
+            this.metadata = {}
           })
 
         if (this.currentSong) {
           this.currentSong.pause()
+          this.currentSong = null
         }
 
         this.currentSong = new Audio()
         this.currentSong.src = this.filePath
+        this.currentSong.onended = this.onPlayEnded
 
         let source = this.audioContext.createMediaElementSource(this.currentSong)
         source.connect(this.audioContext.destination)
@@ -82,13 +85,25 @@
         this.togglePlay()
       },
       togglePlay () {
+        if (!this.currentSong) {
+          return
+        }
+
         if (!this.currentSong.paused) {
           this.currentSong.pause()
         } else {
           this.currentSong.play()
+            .catch(err => {
+              console.log(err)
+              this.currentSong = null
+              this.isPlaying = false
+            })
         }
 
         this.isPlaying = !this.currentSong.paused
+      },
+      onPlayEnded () {
+        this.isPlaying = false
       }
     }
   }
