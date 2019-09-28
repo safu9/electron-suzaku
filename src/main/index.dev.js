415c1c74e3678b634ba6e60d814b95c5d38f015b
safu9 <safu9.dev@gmail.com>
2019-09-28 21:08:02 +0900

    Refactor index.dev.js

--- src/main/index.dev.js
+++ src/main/index.dev.js
@@ -5,14 +5,15 @@
  *  environment.
  */
 
-/* eslint-disable */
+const electron = require('electron')
+const debug = require('electron-debug')
+const installExtension = require('electron-devtools-installer')
 
 // Install `electron-debug` with `devtron`
-require('electron-debug')({ showDevTools: true })
+debug({ showDevTools: true })
 
 // Install `vue-devtools`
-require('electron').app.on('ready', () => {
-  let installExtension = require('electron-devtools-installer')
+electron.app.on('ready', () => {
   installExtension.default(installExtension.VUEJS_DEVTOOLS)
     .then(() => {})
     .catch(err => {
