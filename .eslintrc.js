cfe0eb2c98a4111709aeb2a3f6bebc5d055e7a05
safu9 <safu9.dev@gmail.com>
2019-09-28 18:07:04 +0900

    Update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,20 +9,16 @@ module.exports = {
     node: true
   },
   extends: [
-    'standard', 'plugin:vue/essential'
+    'standard',
+    'plugin:vue/essential'
   ],
   globals: {
     __static: true
   },
   plugins: [
-    'html'
   ],
   rules: {
-    // allow paren-less arrow functions
-    'arrow-parens': 0,
-    // allow async-await
-    'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   }
 }
