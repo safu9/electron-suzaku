146a409c13c78d1cf6624e12d88b5e4d6dde8d19
safu9 <safu9.dev@gmail.com>
2019-09-28 19:20:45 +0900

    Remove build settings for web

--- .electron-vue/webpack.renderer.config.js
+++ .electron-vue/webpack.renderer.config.js
@@ -136,7 +136,7 @@ let rendererConfig = {
   output: {
     filename: '[name].js',
     libraryTarget: 'commonjs2',
-    path: path.join(__dirname, '../dist/electron')
+    path: path.join(__dirname, '../dist')
   },
   resolve: {
     alias: {
@@ -169,7 +169,7 @@ if (process.env.NODE_ENV === 'production') {
     new CopyWebpackPlugin([
       {
         from: path.join(__dirname, '../static'),
-        to: path.join(__dirname, '../dist/electron/static'),
+        to: path.join(__dirname, '../dist/static'),
         ignore: ['.*']
       }
     ]),
